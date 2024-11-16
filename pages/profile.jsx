9fa7eca9c19762ef47b9b4a4d5e9c3ae4b71723a
Kumaraguru <19eucs071@skcet.ac.in>
2024-11-16 22:05:24 +0700

    adding additional ui screens

---
+++ pages/profile.jsx
@@ -0,0 +1,39 @@
+import { useState, useEffect } from 'react'
+import Head from 'next/head'
+import BottomNav from '../components/BottomNav'
+
+export default function Profile() {
+
+  return (
+    <div className="">
+      <Head>
+        <title>FriendCircle</title>
+      </Head>
+      <div>
+        <div className='flex justify-center md:justify-between p-5 items-center'>
+          <div className='hidden md:flex space-x-10'>
+            <h1>team</h1>
+            <h1>github</h1>
+          </div>
+          <img className='w-10 h-10' src='frencircle-dark.png' />
+        </div>
+      </div>
+      <div className='p-10 mb-20'>
+        <h1 className='md:text-4xl text-2xl font-bold mb-10'>profile</h1>
+        <div className='flex justify-center'>
+            <div  className='w-32 h-32 bg-gray-200 rounded-full'></div>
+        </div>
+        <div className='border border-opacity-20 border-white rounded-lg p-5 mt-10'>
+        <h1 className='md:text-4xl text-xl font-bold opacity-50 mb-3'>interests</h1>
+        <div className='flex flex-wrap gap-2'>
+        <div className='bg-white bg-opacity-20 p-1 px-2 rounded-full text-xs'>Football</div>
+        <div className='bg-white bg-opacity-20 p-1 px-2 rounded-full text-xs'>Therapy</div>
+        <div className='bg-white bg-opacity-20 p-1 px-2 rounded-full text-xs'>Yoga</div>
+        </div> 
+        </div>
+</div>
+      
+      <BottomNav />
+    </div>
+  )
+}
