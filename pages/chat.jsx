93ca911fd7d126c967e12c5c62710b949e8621e2
Kumaraguru <19eucs071@skcet.ac.in>
2024-11-17 05:36:59 +0700

    chat

--- pages/chat.jsx
+++ pages/chat.jsx
@@ -35,7 +35,7 @@ export default function Chat() {
         console.log('Starting node...');
         await node.start();
         console.log('Waiting for peers...');
-                await waitForRemotePeer(node, [Protocols.LightPush,Protocols.Filter, Protocols.Store])
+        await waitForRemotePeer(node, [Protocols.LightPush,Protocols.Filter, Protocols.Store])
         console.log('Connected to peers');
         console.log(await node.libp2p.peerStore.all())
         setWakuNode(node);
